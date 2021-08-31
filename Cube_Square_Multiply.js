d0b52e32c2e9ac616d06d6685563265b9e664d81
srujanshenoy <srujanshenoy@gmail.com>
2021-08-31 08:53:44 +0530

    add semicolens

--- Cube_Square_Multiply.js
+++ Cube_Square_Multiply.js
@@ -1,16 +1,16 @@
 function square(_number) {
-    _square = _number * _number
-    return _square
+    _square = _number * _number;
+    return _square;
 }
 
 function cube(_number) {
-    _cube = _number * _number * _number
-    return _cube
+    _cube = _number * _number * _number;
+    return _cube;
 }
 
 function multiply(Number_1, Number_2) {
-    _Multiply = Number_1 * Number_2
-    return _Multiply
+    _Multiply = Number_1 * Number_2;
+    return _Multiply;
 }
 
 function Return_all(Number_to_square_and_cube, Number_to_multiply_with_Number_to_square_and_cube) {
@@ -23,7 +23,7 @@ function Return_all(Number_to_square_and_cube, Number_to_multiply_with_Number_to
     console.log("   Square of \"Number to square and cube\": " + square(Number_to_square_and_cube));
     console.log("   Product of the two numbers you entered: " + multiply(Number_to_square_and_cube, Number_to_multiply_with_Number_to_square_and_cube));
 }
-x = /* Put a number here=>> */ 3
+x = /* Put a number here=>> */ 3;
 y = /*Put a number here =>>*/ 3;
 
 console.log(Return_all(x, y));
